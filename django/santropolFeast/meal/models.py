a75f415c9795fd4b3caf28cb1f6b8d708f4a4672
Emmanuel Milou <manumilou@gmail.com>
2016-02-29 20:43:22 -0500

    Use standart i18n key in meal model

--- django/santropolFeast/meal/models.py
+++ django/santropolFeast/meal/models.py
@@ -3,26 +3,26 @@ from django.utils.translation import ugettext_lazy as _
 
 class Meal(models.Model):
 	class Meta:
-		verbose_name_plural = _('Meals')
+		verbose_name_plural = _('meals')
 
 	#Meal information
-	nom = models.CharField(max_length=50, verbose_name=_('Name'))
-	description = models.TextFields(verbose_name=_('Description'))
+	nom = models.CharField(max_length=50, verbose_name=_('name'))
+	description = models.TextFields(verbose_name=_('description'))
 	ingredients = models.ManyToManyField(Ingredient, related_name='related_meals'))
 
 class Ingredient(models.Model):
         class Meta:
-                verbose_name_plural = _('Ingredients')
+                verbose_name_plural = _('ingredients')
 
         #Ingredient information
-        nom = models.CharField(max_length=50, verbose_name=_('Name'))
+        nom = models.CharField(max_length=50, verbose_name=_('name'))
 
 
 class Allergy(models.Model):
         class Meta:
-                verbose_name_plural = _('Allergies')
+                verbose_name_plural = _('allergies')
 
         #Allergy information
-        nom = models.CharField(max_length=50, verbose_name=_('Name'))
-        description = models.TextFields(verbose_name=_('Description'))
+        nom = models.CharField(max_length=50, verbose_name=_('name'))
+        description = models.TextFields(verbose_name=_('description'))
         ingredients = models.ManyToManyField(Ingredient, related_name='related_allergies'))
