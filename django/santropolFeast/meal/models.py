8fc2044db5c709d5101d86f4da1ff20e3ef87be3
RignonNoel <rignon.noel@openmailbox.org>
2016-03-03 15:45:37 -0500

    standardize and fixs some error in meals model

--- django/santropolFeast/meal/models.py
+++ django/santropolFeast/meal/models.py
@@ -1,28 +1,36 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-class Meal(models.Model):
-	class Meta:
-		verbose_name_plural = _('meals')
 
-	#Meal information
-	nom = models.CharField(max_length=50, verbose_name=_('name'))
-	description = models.TextFields(verbose_name=_('description'))
-	ingredients = models.ManyToManyField(Ingredient, related_name='related_meals'))
+class Meal(models.Model):
+    class Meta:
+        verbose_name_plural = _('meals')
+
+    # Meal information
+    nom = models.CharField(max_length=50, verbose_name=_('name'))
+    description = models.TextField(verbose_name=_('description'))
+    ingredients = models.ManyToManyField(
+        'Ingredient',
+        related_name='related_meals'
+    )
+
 
 class Ingredient(models.Model):
-        class Meta:
-                verbose_name_plural = _('ingredients')
+    class Meta:
+        verbose_name_plural = _('ingredients')
 
-        #Ingredient information
+        # Ingredient information
         nom = models.CharField(max_length=50, verbose_name=_('name'))
 
 
 class Allergy(models.Model):
-        class Meta:
-                verbose_name_plural = _('allergies')
+    class Meta:
+        verbose_name_plural = _('allergies')
 
-        #Allergy information
-        nom = models.CharField(max_length=50, verbose_name=_('name'))
-        description = models.TextFields(verbose_name=_('description'))
-        ingredients = models.ManyToManyField(Ingredient, related_name='related_allergies'))
+    # Allergy information
+    nom = models.CharField(max_length=50, verbose_name=_('name'))
+    description = models.TextFields(verbose_name=_('description'))
+    ingredients = models.ManyToManyField(
+        'Ingredient',
+        related_name='related_allergies'
+    )
