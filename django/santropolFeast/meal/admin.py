d948e215e0cf7a2a18b0b2b2d8d73f740dd0fe20
RignonNoel <rignon.noel@openmailbox.org>
2016-03-03 17:30:15 -0500

    add admin panel for meal application

--- django/santropolFeast/meal/admin.py
+++ django/santropolFeast/meal/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from meal.models import Meal, Allergy, Ingredient
 
-# Register your models here.
+admin.site.register(Meal)
+admin.site.register(Allergy)
+admin.site.register(Ingredient)
