baf6eb158e73ad493bb2f43fc28b99bf17c01648
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2016-04-04 18:26:02 -0400

    Add Notification module

---
+++ django/santropolFeast/notification/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
+class Notification(models.Model):
+
+    class Meta:
+        verbose_name_plural = _('notifications')
+
+    description = models.TextField(
+        verbose_name=_('description')
+    )
+
+    member = models.ForeignKey(
+        'member.Member',
+        verbose_name=_('member')
+    )
+
+    date = models.DateField(
+        auto_now=False,
+        auto_now_add=False,
+        default=timezone.now
+    )
+
+    def __str__(self):
+        return self.description
