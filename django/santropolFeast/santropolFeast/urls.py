d043c26bd7d8185e499a4202a24e7f96ba5072b4
RignonNoel <rignon.noel@openmailbox.org>
2016-04-09 11:16:47 -0400

    outsourcing urls in each app

--- django/santropolFeast/santropolFeast/urls.py
+++ django/santropolFeast/santropolFeast/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^meal/', include('meal.urls', namespace="meal")),
+    url(r'^member/', include('member.urls', namespace="member")),
+    url(r'^notification/', include('notification.urls', namespace="notification")),
+    url(r'^order/', include('order.urls', namespace="order")),
+    url(r'^pages/', include('page.urls', namespace="page")),
 ]
