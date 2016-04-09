c435334898c0df4b889275056095dedc123bc0fd
RignonNoel <rignon.noel@openmailbox.org>
2016-04-09 12:03:29 -0400

    add static file and static settings

--- django/santropolFeast/santropolFeast/settings.py
+++ django/santropolFeast/santropolFeast/settings.py
@@ -152,4 +152,7 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATICFILES_DIRS = (
+    BASE_DIR + '/santropolFeast/static/',
+)
 STATIC_URL = '/static/'
