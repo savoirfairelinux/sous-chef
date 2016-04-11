f8f046c5e03f08485b58f27e8081617059577e00
RignonNoel <rignon.noel@openmailbox.org>
2016-04-11 10:14:33 -0400

    add login and logout page

--- django/santropolFeast/page/views.py
+++ django/santropolFeast/page/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# coding: utf-8
 
-# Create your views here.
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse_lazy
+
+
+def logout_view(request):
+    logout(request)
+    messages.add_message(
+        request,
+        messages.WARNING,
+        _('disconnected message')
+    )
+    # Redirect to a success page.
+    return HttpResponseRedirect(reverse_lazy("page:home"))
--- django/santropolFeast/santropolFeast/settings.py
+++ django/santropolFeast/santropolFeast/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,6 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
                 'NumericPasswordValidator',
     },
 ]
+
+LOGIN_URL = reverse_lazy('page:home')
+LOGIN_REDIRECT_URL = reverse_lazy('page:home')
 
 
 # Internationalization
