13c839c015a3ac525d0eb9b564a113196a4c78e7
RignonNoel <rignon.noel@openmailbox.org>
2016-04-15 11:54:46 -0400

    add ClientList

--- django/santropolFeast/member/views.py
+++ django/santropolFeast/member/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+# coding: utf-8
 
-# Create your views here.
+from django.views import generic
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+class ClientList(generic.ListView):
+    # Display the list of clients
+    template_name = 'client/list.html'
+    context_object_name = 'client'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ClientList, self).dispatch(*args, **kwargs)
+
+    def get_queryset(self):
+        # Here you need to return the list of clients
+        return 0
+
+    def get_context_data(self, **kwargs):
+        context = super(ClientList, self).get_context_data(**kwargs)
+
+        # Here you add some variable of context to display on template
+        context['myVariableOfContext'] = 0
+
+        return context
