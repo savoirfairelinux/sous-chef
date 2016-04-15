8c9df8da6e7f044747e65cd170eb29158b486d03
RignonNoel <rignon.noel@openmailbox.org>
2016-04-15 11:55:12 -0400

    add ClientView

--- django/santropolFeast/member/views.py
+++ django/santropolFeast/member/views.py
@@ -3,6 +3,7 @@
 from django.views import generic
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from member.models import Client
 
 
 class ClientList(generic.ListView):
@@ -20,6 +21,24 @@ class ClientList(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ClientList, self).get_context_data(**kwargs)
+
+        # Here you add some variable of context to display on template
+        context['myVariableOfContext'] = 0
+
+        return context
+
+
+class ClientView(generic.DetailView):
+    # Display detail of one client
+    model = Client
+    template_name = 'client/detail.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ClientView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ClientView, self).get_context_data(**kwargs)
 
         # Here you add some variable of context to display on template
         context['myVariableOfContext'] = 0
