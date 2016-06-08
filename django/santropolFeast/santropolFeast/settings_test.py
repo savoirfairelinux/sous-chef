474c37392f3370bdea6c84ea1a61ebe408a04066
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-06-08 13:11:48 -0400

    Update the test for member

--- django/santropolFeast/santropolFeast/settings_test.py
+++ django/santropolFeast/santropolFeast/settings_test.py
@@ -21,8 +21,46 @@ INSTALLED_APPS = [
     'member',
     'order',
     'notification',
-    'page'
+    'page',
+    'formtools',
+    'django_filters',
 ]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR + '/santropolFeast/templates/'
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATICFILES_DIRS = (
+    BASE_DIR + '/santropolFeast/static/',
+)
+STATIC_URL = '/static/'
 
 DATABASES = {
     'default': {
