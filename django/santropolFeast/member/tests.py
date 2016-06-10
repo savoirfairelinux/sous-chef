e222d23062cdf4a86cbdd816fa7a74ca60a93e3b
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-06-09 20:24:32 -0400

    add test page to be run on coverage

--- django/santropolFeast/member/tests.py
+++ django/santropolFeast/member/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from member.models import Member, Client, Note, User, Address, Referencing
 from member.models import Contact, Option, Client_option, Restriction
 from member.models import Client_avoid_ingredient, Client_avoid_component
@@ -311,6 +311,7 @@ class ClientAvoidComponentTestCase(TestCase):
         self.assertTrue(component.name in str(client_avoid_component))
 
 class FormTestCase(TestCase):
+
     @classmethod
     def setUpTestData(cls):
         cls.admin = User.objects.create_superuser(
@@ -323,7 +324,7 @@ class FormTestCase(TestCase):
         """Test if the form is accesible from its url"""
         self.client.login(
             username=self.admin.username,
-            password='test1234'
+            password=self.admin.password
         )
         result = self.client.get(
             reverse_lazy(
@@ -332,9 +333,265 @@ class FormTestCase(TestCase):
         )
         self.assertEqual(result.status_code, 200)
 
-    def test_form(self):
-        """Test if the form data is correct"""
-        pass
+    def test_acces_to_form_by_url_basic_info(self):
+
+        result = self.client.get('/member/create/basic_info', follow=True)
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_acces_to_form_by_url_adress_information(self):
+
+        result = self.client.get(
+            '/member/create/address_information', follow=True
+        )
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_acces_to_form_by_url_referent_information(self):
+
+        result = self.client.get(
+            '/member/create/referent_information', follow=True
+        )
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_acces_to_form_by_url_payment_information(self):
+
+        result = self.client.get(
+            '/member/create/referent_infomation', follow=True
+        )
+        self.assertEqual(result.status_code, 200)
+
+    def test_acces_to_form_by_url_dietary_restriction(self):
+
+        result = self.client.get(
+            '/member/create/dietary_restriction', follow=True
+        )
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_acces_to_form_by_url_emergency_contact(self):
+
+        result = self.client.get(
+            '/member/create/emergency_contact', follow=True
+        )
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_form_save_data(self):
+
+        basic_information_request = {
+            "client_wizard-current_step": "basic_info",
+            "basic_info-firstname": "Testing",
+            "basic_info-lastname": "User",
+            "basic_info-languages": "fr",
+            "basic_info-gender": "M",
+            "basic_info-birthdate": "12/12/1995",
+            "basic_info-contact_type": "Home phone",
+            "basic_info-contact_value": "555-555-5555",
+            "basic_info-alert": "Testing alert message",
+            "wizard_goto_step": ""
+
+        }
+
+        address_information_request = {
+            "client_wizard-current_step": "address_information",
+            "address_information-street": "555 rue clark",
+            "address_information-apartement": "222",
+            "address_information-city": "montreal",
+            "address_information-postal_code": "H3C2C2",
+            "wizard_goto_step": "",
+        }
+
+        referent_information_request = {
+            "client_wizard-current_step": "referent_information",
+            "referent_information-firstname": "Referent",
+            "referent_information-lastname": "Testing",
+            "referent_information-work_information": "CLSC",
+            "referent_information-date": "12/12/2012",
+            "referent_information-referral_reason": "Testing referral reason",
+            "wizard_goto_step": "",
+        }
+
+        billing_information_request = {
+            "client_wizard-current_step": "billing_information",
+            "billing_information-firstname": "Testing",
+            "billing_information-lastname": "Billing",
+            "billing_information-billing_payment_type": "check",
+            "billing_information-facturation": "default",
+            "billing_information-street": "111 rue clark",
+            "billing_information-apartement": "222",
+            "billing_information-city": "Montreal", "postal_code": "H2C3G4",
+            "wizard_goto_step": "",
+        }
+
+        restriction_information_request = {
+            "client_wizard-current_step": "dietary_restriction",
+            "dietary_restriction-status": "on",
+            "dietary_restriction-delivery_type": "O",
+            "wizard_goto_step": ""
+        }
+
+        emergency_contact_request = {
+            "client_wizard-current_step": "emergency_contact",
+            "emergency_contact-firstname": "Emergency",
+            "emergency_contact-lastname": "User",
+            "emergency_contact-contact_type": "Home phone",
+            "emergency_contact-contact_value": "555-444-5555"
+        }
+
+        self.client.post(
+            '/member/create/basic_information', basic_information_request
+        )
+
+        self.client.post(
+            '/member/create/address_information', address_information_request,
+        )
+
+        self.client.post(
+            '/member/create/referent_information',
+            referent_information_request
+        )
+
+        self.client.post(
+            '/member/create/billing_information', billing_information_request,
+        )
+
+        self.client.post(
+            '/member/create/dietary_restriction',
+            restriction_information_request
+         )
+
+        self.client.post(
+            '/member/create/emergency_contact', emergency_contact_request,
+        )
+
+        print (Member.objects.all)
+        member = Member.objects.get(firstname="Testing")
+
+    def test_member_name(self):
+
+        member = Member.objets.get(firstname="Testing")
+        self.assertTrue(member.firstname, "Testing")
+        self.assertTrue(member.lastname, "User")
+
+    def test_home_phone_member(self):
+
+        member = Member.objects.get(firstname="Testing")
+        self.assertTrue(member.get_home_phone().value.startswith('555'))
+
+    def test_member_alert(self):
+
+        member = Member.objects.get(firstname="Testing")
+        self.assertTrue(member.alert, "Testing alert message")
+
+    def test_client_languages(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+        self.assertTrue(client.language, "fr")
+
+    def test_client_birthdate(self):
+
+        member = Member.objets.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+        self.assertTrue(client.birthdate, "12/12/1995")
+
+    def test_client_gender(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+        self.assertTrue(client.gender, "M")
+
+    def test_client_contact_type(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+        self.assertTrue(member.contact.contact_type, "Home phone")
+
+    def test_client_address(self):
+
+        member = Member.objects.get(firstname="Testing")
+        self.assertTrue(member.address.street, "555 rue clark")
+        self.assertTrue(member.address.postal_code, "H3C2C2")
+        self.assertTrue(member.address.apartment, "222")
+        self.assertTrue(member.address.city, "montreal")
+
+    def test_referent_name(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue(client.client_referent.referent.firstname, "Referent")
+        self.assertTrue(client.client_referent.referent.lastname, "Testing")
+
+    def test_referent_work_information(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue(client.client_referent.work_information, "CLSC")
+
+    def test_referral_date(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue(client.client_referent.date, "12/12/2012")
+
+    def test_referral_reason(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue(
+            client.client_referent.referral_reason, "Testing referral reason"
+        )
+
+    def test_billing_name(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue(client.billing_member.firstname, "Testing")
+        self.assertTrue(client.billing_member.lastname, "Billing")
+
+    def test_billing_type(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue(client.billing_payment_type, "check")
+
+    def test_billing_address(self):
+        # missing models for billing address
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+    def test_billing_rate_type(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue(client.rate_type, "default")
+
+    def test_emergency_contact_name(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue(client.emergency_contact.firstname, "Emergency")
+        self.assertTrue(client.emergency_contact.lastname, "User")
+
+    def test_emergency_contact_value(self):
+
+        member = Member.objects.get(firstname="Testing")
+        client = Client.objects.get(Member=member)
+
+        self.assertTrue
+        (
+            client.emergency_contact.get_home_phone, "555-444-5555"
+        )
 
     def tear_down(self):
         self.client.logout()
