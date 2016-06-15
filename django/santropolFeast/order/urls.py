811d487a41711185ecaf5442e91a30845e595c5b
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-06-15 13:34:58 -0400

    add a listing of all the order made add a page that contains a list of all the order that were made with there creation date, delivery date, client name, status and Total Price for the order

--- django/santropolFeast/order/urls.py
+++ django/santropolFeast/order/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.utils.translation import ugettext_lazy as _
 
-from order.views import show_order_information
+from order.views import show_order_information, OrderList
 
 urlpatterns = [
-    url(_(r'^view/(?P<id>\d+)/$'), show_order_information, name='view')
+    url(_(r'^view/(?P<id>\d+)/$'), show_order_information, name='view'),
+    url(_(r'^list/$'), OrderList.as_view(), name='list')
 ]
--- django/santropolFeast/order/views.py
+++ django/santropolFeast/order/views.py
@@ -3,6 +3,9 @@ from django.views import generic
 from django.utils.decorators import method_decorator
 from django.shortcuts import get_object_or_404
 from member.models import Client
+from order.models import Order
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 def show_order_information(request, id):
@@ -11,3 +14,14 @@ def show_order_information(request, id):
     return render(request, 'list_order.html',
                   {'client': client}
                   )
+
+
+class OrderList(generic.ListView):
+    # Display the list of clients
+    model = Order
+    template_name = 'list_all_order.html'
+    context_object_name = 'order'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(OrderList, self).dispatch(*args, **kwargs)
