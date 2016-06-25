d547914d9c0c444e8fd0f0b07ed841545ac9372d
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2016-06-25 01:07:22 -0400

    Work on client record

--- django/santropolFeast/page/views.py
+++ django/santropolFeast/page/views.py
@@ -9,13 +9,25 @@ from django.core.urlresolvers import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from member.models import Note
+from member.models import Note, Client
+from datetime import datetime, timedelta
+from functools import reduce
+import operator
 
 
 @login_required
 def home(request):
     notes = list(Note.objects.all())
-    return render(request, 'pages/home.html', {'notes': notes})
+    active_clients = Client.objects.filter(status='A').count()
+    pending_clients = Client.objects.filter(status='D').count()
+    clients = Client.objects.all()
+    #birthday = birthdays_within(1)
+    return render(request, 'pages/home.html', {
+        'notes': notes,
+        'active_clients': active_clients,
+        'pending_clients': pending_clients,
+        'birthday': clients,
+    })
 
 
 def custom_login(request):
@@ -24,3 +36,25 @@ def custom_login(request):
         return HttpResponseRedirect(reverse_lazy("page:home"))
     else:
         return login(request)
+
+def birthdays_within(days):
+
+    now = datetime.now()
+    then = now + timedelta(days)
+
+    # Build the list of month/day tuples.
+    monthdays = [(now.month, now.day)]
+    while now <= then:
+        monthdays.append((now.month, now.day))
+        now += timedelta(days=1)
+
+    # Tranform each into queryset keyword args.
+    monthdays = (dict(zip(("birthdate__month", "birthdate__day"), t))
+                 for t in monthdays)
+
+
+    # Compose the djano.db.models.Q objects together for a single query.
+    query = reduce(operator.or_, (Client(**d) for d in monthdays))
+
+    # Run the query.
+    return Client.objects.filter(query)
