1a9b276b2bc84fa58e9000923c6943940978a077
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-29 20:33:31 -0400

    DOC: In docs/conf.py, setting up Django in order to solve Apps are not installed.

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,10 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../django/SantropolFeast'))
 
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "santropolFeast.settings")
+django.setup()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
