c10848939b245537e5b0774ecc5a740fc37a14a8
David Cormier <david@cormier.xyz>
2016-07-01 18:53:13 -0400

    remove spurious AllergyFactory
    There is no Allergy model.

--- django/santropolFeast/meal/factories.py
+++ django/santropolFeast/meal/factories.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 import factory
-from meal.models import Ingredient, Allergy
+from meal.models import Ingredient
 
 
 class IngredientFactory(factory.DjangoModelFactory):
@@ -15,17 +15,3 @@ class IngredientFactory(factory.DjangoModelFactory):
         name = kwargs.pop('name', None)
         ingredients = super(IngredientFactory, self).__init__(self, **kwargs)
         ingredients.save()
-
-
-class AllergyFactory(factory.DjangoModelFactory):
-    class Meta:
-        model = Allergy
-
-    name = "Tomato"
-    description = "A Simple Tomato"
-
-    @classmethod
-    def __init__(self, **kwargs):
-        name = kwargs.pop("name", None)
-        allergy = super(AllergyFactory, self).__init__(self, **kwargs)
-        allergy.save()
