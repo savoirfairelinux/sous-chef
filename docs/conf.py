001388d2d2287ab96730a3219ff6292e08aac5ff
David Cormier <david@cormier.xyz>
2016-07-01 18:35:10 -0400

    docs: do not capitalize project name
    Capitalizing the project name breaks the documentation build
    on systems where paths are case sensitive.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../django/SantropolFeast'))
+sys.path.insert(0, os.path.abspath('../django/santropolFeast'))
 
 import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "santropolFeast.settings")
