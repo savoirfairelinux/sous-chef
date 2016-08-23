c69da19e8316b1e1c6f55d3571917e85ba4fbf8d
David Cormier <david@cormier.xyz>
2016-08-22 21:57:19 -0400

    Add ingredients to avoid as a TabularInline to Client
    Do not edit the ingredients to avoid directly, edit them through
    a tabular form from the client form

--- src/member/admin.py
+++ src/member/admin.py
@@ -4,14 +4,27 @@ from member.models import (Member, Client, Contact, Address,
                            Client_avoid_ingredient, Option,
                            Client_option, Restriction)
 
+from meal.models import Ingredient
+
+
+class IngredientsToAvoidInline(admin.TabularInline):
+    model = Client.ingredients_to_avoid.through
+
+
+class ClientAdmin(admin.ModelAdmin):
+
+    inlines = [
+        IngredientsToAvoidInline
+    ]
+
+
 admin.site.register(Member)
-admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
 admin.site.register(Route)
 admin.site.register(Contact)
 admin.site.register(Address)
 admin.site.register(Referencing)
 admin.site.register(Client_avoid_component)
-admin.site.register(Client_avoid_ingredient)
 admin.site.register(Option)
 admin.site.register(Client_option)
 admin.site.register(Restriction)
