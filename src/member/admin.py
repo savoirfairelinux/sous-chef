9490be22184bbac09fb52871c50bb96fdb4b7515
David Cormier <david@cormier.xyz>
2016-08-23 10:25:49 -0400

    replace client_avoid_component form with tabular inline

--- src/member/admin.py
+++ src/member/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from member.models import (Member, Client, Contact, Address,
-                           Referencing, Route, Client_avoid_component,
-                           Client_avoid_ingredient, Option,
+                           Referencing, Route, Option,
                            Client_option, Restriction)
 
 from meal.models import Ingredient
@@ -11,9 +10,14 @@ class IngredientsToAvoidInline(admin.TabularInline):
     model = Client.ingredients_to_avoid.through
 
 
+class ComponentsToAvoidInline(admin.TabularInline):
+    model = Client.components_to_avoid.through
+
+
 class ClientAdmin(admin.ModelAdmin):
 
     inlines = [
+        ComponentsToAvoidInline,
         IngredientsToAvoidInline
     ]
 
@@ -24,7 +28,6 @@ admin.site.register(Route)
 admin.site.register(Contact)
 admin.site.register(Address)
 admin.site.register(Referencing)
-admin.site.register(Client_avoid_component)
 admin.site.register(Option)
 admin.site.register(Client_option)
 admin.site.register(Restriction)
