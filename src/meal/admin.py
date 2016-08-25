b6bbb7feb5cf47de782bba93aadbd287ee1a156f
David Cormier <david@cormier.xyz>
2016-08-25 19:50:02 -0400

    Add components inline admin to menu

--- src/meal/admin.py
+++ src/meal/admin.py
@@ -3,10 +3,21 @@ from meal.models import Component, Restricted_item
 from meal.models import Ingredient, Component_ingredient
 from meal.models import Incompatibility, Menu, Menu_component
 
+
+class ComponentsInline(admin.TabularInline):
+    model = Menu.components.through
+
+
+class MenuAdmin(admin.ModelAdmin):
+
+    inlines = [
+        ComponentsInline
+    ]
+
+
 admin.site.register(Component)
 admin.site.register(Restricted_item)
 admin.site.register(Ingredient)
 admin.site.register(Component_ingredient)
 admin.site.register(Incompatibility)
-admin.site.register(Menu)
-admin.site.register(Menu_component)
+admin.site.register(Menu, MenuAdmin)
