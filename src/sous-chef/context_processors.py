64998ae001ec3221b43d5cd953d843b30a6e6900
Benoit Grégoire <benoitg@coeus.ca>
2016-08-26 00:18:56 -0400

    Implement default filters in main menu for clients (#379), orders (#378), and staff notes (#389)

--- src/sous-chef/context_processors.py
+++ src/sous-chef/context_processors.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from member.models import Client
-from order.models import Order
-from note.models import Note
+from order.models import Order, ORDER_STATUS_ORDERED
+from note.models import Note, NoteFilter
+import datetime
 
 
 def total(request):
@@ -17,6 +18,14 @@ def total(request):
         'CLIENTS_TOTAL': clients,
         'ORDERS_TOTAL': orders,
         'NOTES_TOTAL': notes,
+        # This is pretty bad separation of concert to puth the following here
+        # But since there is no actual view associated with menu.html (it's
+        # just included is base.html, this is the most DRY way I found
+        'CLIENT_FILTER_DEFAULT_STATUS': Client.ACTIVE,
+        'ORDER_FILTER_DEFAULT_STATUS': ORDER_STATUS_ORDERED,
+        'ORDER_FILTER_DEFAULT_DATE': datetime.datetime.now(),
+        'NOTE_FILTER_DEFAULT_IS_READ': NoteFilter.NOTE_STATUS_UNREAD
+        
     }
 
     return COMMON_CONTEXT
