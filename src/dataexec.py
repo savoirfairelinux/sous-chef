da39f0fdbbc0d7b9af132ddc0a0f59d15101839e
Francois Lamontagne <francois.r.lamontagne@gmail.com>
2016-09-05 15:44:55 -0400

    all ingredients, recipes and restriction data

---
+++ src/dataexec.py
@@ -0,0 +1,17 @@
+# usage : python dataexec.py [santropolFeast.settingsSPECIAL]
+import os
+import sys
+
+
+def run():
+    if len(sys.argv) > 1:
+        settings = sys.argv[1]
+    else:
+        settings = 'santropolFeast.settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = settings
+    import django
+    django.setup()
+    from dataload import insert_all
+    insert_all()
+
+run()
