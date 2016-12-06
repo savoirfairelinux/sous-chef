3e236f316fb3807b2da1891f6b3e42ffff3191d3
Ling-Xiao Yang <ling-xiao.yang@savoirfairelinux.com>
2016-12-06 10:47:02 -0500

    Fix: forgotten new file

---
+++ src/meal/settings.py
@@ -0,0 +1,11 @@
+COMPONENT_SYSTEM_DEFAULT = {
+    'main_dish': 1,
+    'size': 'R',
+    'dessert': 1,
+    'diabetic': 0,
+    'fruit_salad': 0,
+    'green_salad': 0,
+    'pudding': 0,
+    'compote': 0,
+    'sides': 0
+}
