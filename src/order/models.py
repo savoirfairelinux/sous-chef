4bf69c9318fec7009bb9187775658c3de8173750
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2017-01-26 13:56:29 -0500

    Allow batch order status change

--- src/order/models.py
+++ src/order/models.py
@@ -328,6 +328,13 @@ class OrderManager(models.Manager):
 
         return order
 
+    """
+    Allow changing status of multiple orders at once.
+    """
+    def update_orders_status(self, orders, new):
+        count = orders.update(status=new)
+        return count
+
 
 class Order(models.Model):
 
