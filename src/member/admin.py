2c31e227377c958eea0baf5042221dbc977ada6b
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2017-01-26 17:58:38 -0500

    Improve admin interface

--- src/member/admin.py
+++ src/member/admin.py
@@ -21,9 +21,38 @@ class OptionsInline(admin.TabularInline):
     model = Client.options.through
 
 
-class ClientAdmin(admin.ModelAdmin):
+class ContactInline(admin.TabularInline):
+    model = Contact
 
+
+class ReferencingInline(admin.TabularInline):
+    model = Referencing
+
+
+class MemberAdmin(admin.ModelAdmin):
+    search_fields = ['lastname', 'firstname']
+    list_display = ('full_name', 'address', 'work_information', 'updated_at')
     inlines = [
+        ContactInline
+    ]
+
+    def full_name(self, obj):
+        return ("%s %s" % (obj.firstname, obj.lastname))
+    full_name.short_description = 'Name'
+
+
+class ClientAdmin(admin.ModelAdmin):
+    search_fields = ['member__lastname', 'member__firstname']
+    list_filter = ('status', 'route', 'delivery_type')
+    list_display = (
+        'member',
+        'status',
+        'language',
+        'delivery_type',
+        'gender',
+        'route')
+    inlines = [
+        ReferencingInline,
         OptionsInline,
         RestrictionsInline,
         ComponentsToAvoidInline,
@@ -31,10 +60,21 @@ class ClientAdmin(admin.ModelAdmin):
     ]
 
 
-admin.site.register(Member)
+class ContactAdmin(admin.ModelAdmin):
+    search_fields = ['member__lastname', 'member__firstname']
+    list_display = ('member', 'type', 'value', )
+    list_filter = ('type',)
+
+
+class ReferencingAdmin(admin.ModelAdmin):
+    search_fields = ['referent__lastname', 'referent__firstname']
+    list_display = ('referent', 'client', 'referral_reason', 'date')
+
+
+admin.site.register(Member, MemberAdmin)
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Route)
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
 admin.site.register(Address)
-admin.site.register(Referencing)
+admin.site.register(Referencing, ReferencingAdmin)
 admin.site.register(Option)
