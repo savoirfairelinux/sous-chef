1fb059cf9a6f56a6492fcc96cbfeb65c94a4c59a
Adonys Alea Boffill <adonys.boffill@savoirfairelinux.com>
2017-03-21 09:12:33 -0400

    Fixed #672: Added placeholder to member's field in client form

--- src/member/forms.py
+++ src/member/forms.py
@@ -250,7 +250,7 @@ class MemberForm(forms.Form):
     member = forms.CharField(
         label=_("Member"),
         widget=forms.TextInput(attrs={
-            'placeholder': _('Member'),
+            'placeholder': _('Type more than 2 characters to search Members'),
             'class': 'prompt existing--member'
         }),
         required=False
