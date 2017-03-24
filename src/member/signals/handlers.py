804ea7727d221921b1ef1f8ff27fbce8b73b39b2
Adonys Alea Boffill <adonys.boffill@savoirfairelinux.com>
2017-03-24 15:02:27 -0400

    Fixed #685: Added dispatch_uid for the receiver

--- src/member/signals/handlers.py
+++ src/member/signals/handlers.py
@@ -4,7 +4,11 @@ from django.dispatch import receiver
 from ..models import ClientScheduledStatus
 
 
-@receiver(post_delete, sender=ClientScheduledStatus)
+@receiver(
+    post_delete,
+    sender=ClientScheduledStatus,
+    dispatch_uid="post_delete.ensure_pair_remove"
+)
 def ensure_pair_remove(sender, instance, **kwargs):
     if instance.get_pair:
         ClientScheduledStatus.objects.get(pk=instance.get_pair.pk).delete()
